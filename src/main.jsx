5c335a6f4e2a7d59d9dae966802f8e05ffda0fc5
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-07 22:09:30 +0545

    updated the ui and removed unused files

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { JobContextProvider } from "./components/DataContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <JobContextProvider>
-      <App />
-    </JobContextProvider>
+    <App />
   </StrictMode>
 );
